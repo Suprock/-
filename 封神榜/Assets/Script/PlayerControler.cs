be708528736f402286dc8507e12ef6220ce45dac
suprock <suprock@163.com>
2020-09-30 16:36:26 +0800

    添加人物属性以及初步UI
    1. 代码层面添加人物属性
    2. 人物属性界面初步添加

--- 封神榜/Assets/Script/PlayerControler.cs
+++ 封神榜/Assets/Script/PlayerControler.cs
@@ -10,11 +10,26 @@ public class PlayerControler : MonoBehaviour
 
     private Vector3 point;
     // Start is called before the first frame update
+    private int mask;
     void Start()
     {
         direction = Vector2.zero;
         speed = 0.04f;
         anim = gameObject.GetComponent<Animator>();
+        //接受开始或者读档场景传递的数据
+        //读档数据要把属性都传递过来。json数据以mask为标准判定是读档或是新的开始
+        //如{"mask":1, "archive":1}
+        //如{"mask":0}
+        mask = 0;
+        if(mask == 0)
+        {
+            PlayerProperty playerNezha = new PlayerProperty("nezha");
+            playerNezha.InitZeroProperty();
+        }
+        else if(mask == 1)
+        {
+            //根据archive内容设置不同主角信息
+        }
     }
 
     // Update is called once per frame
---
+++ 封神榜/Assets/Script/PlayerProperty.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerProperty : MonoBehaviour
+{
+    [Header("人物属性")]
+    private string name;
+    private int grade;
+    private int maxHealthValue;
+    private int healthValue;
+    private int maxMagicalValue;
+    private int magicalValue;
+
+    private int attackPower;
+    private int defense;
+    private int spiritualPower;
+    private int magicDefense;
+    private int speed;
+    private int avoidance;
+
+    [Header("携带装备")]
+    //装备编号
+    private int weapon01;
+    private int weapon02;
+    private int clothes;
+    private int shoes;
+    private int necklace;
+
+    [Header("坐标")]
+    //人物位置的坐标值
+    private Vector2 position;
+
+    public PlayerProperty(string lname)
+    {
+        name = lname;
+    }
+    // Start is called before the first frame update
+    void GetPropertyInArchive()
+    {
+        //在存档中获取所有信息
+    }
+
+    public void InitZeroProperty()
+    {
+        //设置主角0J的属性
+        // int zeroMaxHealthValue = 150;
+        // int zeroMaxMagicalValue = 150;
+
+        // int zeroAttackPower = 5;
+        // int zeroDefense = 5;
+        // int zeroSpiritualPower = 5;
+        // int zeroMagicDefense = 5;
+        // int zeroSpeed = 5;
+        switch(name)
+        {
+            case "nezha":
+                grade = 0;
+                healthValue = maxHealthValue = 150;
+                magicalValue = maxMagicalValue = 150;
+                attackPower = 15;
+                defense = 10;
+                spiritualPower = 5;
+                magicDefense = 5;
+                speed = 5;
+                avoidance = 0;
+                weapon01 = 0;
+                weapon02 = 0;
+                clothes = 0;
+                necklace = 0;
+                shoes = 0;
+                break;
+        }
+    }
+
+
+
+
+}
